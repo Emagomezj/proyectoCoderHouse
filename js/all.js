620c26c166c018dc45ab697e81425ab7fa66dea2
Emagomezj <emagomezj@gmail.com>
2024-02-11 13:51:52 -0300

    arreglo ruta de imagen en all.js

--- js/all.js
+++ js/all.js
@@ -4,15 +4,27 @@ const logHeader_icon = document.querySelector('#a_logHeader');
 console.log(window.location.pathname)
 
 if(localStorage.getItem('name')){
-  logHeader.innerHTML = `
-  <button id="btnDdwn" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-  <img src="../img/user.svg" alt="user icon" class="logoUsr" width="40px" style="padding-bottom:10px">
-  </button>
-  <div id="divDdwn" class="dropdown">
-  <ul id='ulDdwn' class="dropdown-menu" style="margin-right: 20px">
-    <button id="cerrSesBtn"> Cerrar sesión</button>
-  </ul>
-  </div>`
+  if(window.location.pathname == '/proyectoCoderHouse/index.html' ){
+    logHeader.innerHTML = `
+    <button id="btnDdwn" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <img src="./img/user.svg" alt="user icon" class="logoUsr" width="40px" style="padding-bottom:10px">
+    </button>
+    <div id="divDdwn" class="dropdown">
+    <ul id='ulDdwn' class="dropdown-menu" style="margin-right: 20px">
+      <button id="cerrSesBtn"> Cerrar sesión</button>
+    </ul>
+    </div>`
+  } else {
+    logHeader.innerHTML = `
+    <button id="btnDdwn" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <img src="../img/user.svg" alt="user icon" class="logoUsr" width="40px" style="padding-bottom:10px">
+    </button>
+    <div id="divDdwn" class="dropdown">
+    <ul id='ulDdwn' class="dropdown-menu" style="margin-right: 20px">
+      <button id="cerrSesBtn"> Cerrar sesión</button>
+    </ul>
+    </div>`
+  }
   const cerrSesBtn = document.querySelector('#cerrSesBtn');
   const btnDdwn = document.querySelector('#btnDdwn');
   const divDdwn = document.querySelector('#divDdwn');
